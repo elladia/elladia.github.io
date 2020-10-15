1a99d6ee067b8a25d885e85a73c8938de39f550a
elladia <68752179+elladia@users.noreply.github.com>
2020-10-15 14:23:44 -0500

    numerology part 1

--- js/numerology.js
+++ js/numerology.js
@@ -1,66 +1,74 @@
-var month = "02";
-var date = "26";
-var year = "1997";
-
-
-function partA(){
-    var a = parseInt(month.slice(0,1));
-    var b = parseInt(month.slice(1,2));
-    
-    var c = parseInt(date.slice(0,1));
-    var d = parseInt(date.slice(1,2));
-    
-    var p = parseInt(year.slice(0,1));
-    var f = parseInt(year.slice(1,2));
-    var g = parseInt(year.slice(2,3));
-    var h = parseInt(year.slice(3,4));
-    
-    var addMonth = a + b;
-    var addDate = c + d;
-    var addYear = p + f + g + h;
-
-    var addNumbers = addMonth + addDate + addYear;
-
-    return addNumbers;
-console.log(addNumbers);
-}; 
-
-
-
-//function partB(){
-//    
-//};
+//var month = "02";
+//var date = "26";
+//var year = "1997";
 
 var UIController = (function() {
     
+    var DOMStrings = {
+        inputDay: "day",
+        inputMonth: "month",
+        inputYear: "year"
+    }
+    
     return {
-        getinput: function() {
+        getInput: function() {
             return {
-            monthA: document.getElementById(".month").value,
-            dayA: document.getElementById(".day").value,
-            yearA: document.getElementById(".year").value
+            day: +document.getElementById(DOMStrings.inputDay).value,
+            month: +document.getElementById(DOMStrings.inputMonth).value,
+            year: +document.getElementById(DOMStrings.inputYear).value
             }  
         }
     };
-    console.log(monthA);
     
 })();
 
+var input = UIController.getInput();
 
-
-
-var controller = (function(UIController, partA) {
-   
-   
-//    document.querySelector('.calc_num').addEventListener('click', function() {
+//var calculateA = function(){
+//    var c = parseInt(input.day.slice(0,1)) | 0;
+//    var d = parseInt(input.day.slice(1,2));
 //    
-//   document.querySelector(".life_path_number").innerHTML = "calculated";
-//})();
-    
+//    var a = parseInt(input.month.slice(0,1)) | 0;
+//    var b = parseInt(input.month.slice(1,2));
+//
+//    var p = parseInt(input.year.slice(0,1));
+//    var f = parseInt(input.year.slice(1,2));
+//    var g = parseInt(input.year.slice(2,3));
+//    var h = parseInt(input.year.slice(3,4));
+//    
+//    var addMonth = a + b;
+//    var addDate = c + d;
+//    var addYear = p + f + g + h;
+//
+//    return {
+//        function addNumbers() = addMonth + addDate + addYear;
+//    }
+//}; 
 
-    
+//var addA = calculateA.addNumbers();
+
+document.querySelector(".calc_num").addEventListener("click", function() {
+//    console.log(addA);
+    console.log(input);
+    document.querySelector(".life_path_number").innerHTML = "Life Path " + input.month;
 });
 
-$(".calc_num").click(function() {
-    document.querySelector(".life_path_number").innerHTML = "calculated";
-});
+
+
+//console.log(input);
+
+//var controller = (function(UIController, partA) {
+//   
+//   
+////    document.querySelector('.calc_num').addEventListener('click', function() {
+////    
+////   document.querySelector(".life_path_number").innerHTML = "calculated";
+////})();
+//    
+//
+//    
+//});
+
+//$(".calc_num").click(function() {
+//    document.querySelector(".life_path_number").innerHTML = "calculated";
+//});
